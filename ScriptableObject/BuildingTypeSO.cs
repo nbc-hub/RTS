b16349fea1bf80b4304599f685e4d0093d624553
Bürhan Çakıcı <74993460+nbc-hub@users.noreply.github.com>
2023-07-25 05:48:09 +0300

    ScriptableObject

---
+++ ScriptableObject/BuildingTypeSO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class BuildingTypeSO : ScriptableObject {
+
+    public Transform prefab;
+    public Transform visual;
+
+    public float constructionProgressMax;
+    public float constructionDistanceOffset;
+    public List<ResourceAmount> constructionResourceAmountCostList;
+}
---
+++ ScriptableObject/UnitTypeSO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class UnitTypeSO : ScriptableObject {
+
+    public Transform prefab;
+    public Sprite icon;
+
+    public float constructionTimerMax;
+    public List<ResourceAmount> constructionResourceAmountCostList;
+
+}
